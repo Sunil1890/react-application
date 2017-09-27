921206cdafa85ee59f17f522af3dc427c57886f4
cc <cc@ccs-iMac.local>
2017-09-27 13:37:14 +0530

    added react-slick image slider

--- client/components/shows.js
+++ client/components/shows.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react"
-
+import SimpleSlider from "./simpleSlider"
 class Shows extends Component {
 	render(){
 		return (
 		<div>
     	<h2>shows</h2>
+    	<SimpleSlider/>
   	</div>
 		)
 	}
---
+++ client/components/simpleSlider.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+import Slider from "react-slick"
+
+class SimpleSlider extends Component {
+  render () {
+    let settings = {
+      dots: true,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      // autoplay:true
+    };
+    return (
+      <Slider {...settings}>
+        <div><img src='http://placekitten.com/g/400/200' /></div>
+        <div><h3>2</h3></div>
+      </Slider>
+    );
+  }
+}
+export default SimpleSlider;
